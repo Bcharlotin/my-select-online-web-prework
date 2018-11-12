e723305c6fae2f45e82cdfbf7bbc66e4d838881d
biancacharlotin <biancacharlotin@gmail.com>
2018-11-12 21:38:31 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -11,3 +11,29 @@ end
 
   empty_arr.compact 
 end 
+
+
+my_select(nums) do |num| 
+  if num.even?
+    num 
+  end 
+end 
+
+nums = [1, 2, 3, 4, 5] 
+
+def my_select(array) 
+  i =0 
+  select = [] 
+  while i < array.length 
+    if (yield(array[i]))
+      # select.push(array[i])OR 
+      select << array[i] 
+    end 
+    i += 1 
+  end 
+  select 
+end 
+
+my_select(nums) do |num| 
+  num.even? 
+end 
